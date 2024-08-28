6a39620b87b7183c1d4bcc06b8c500df82c35675
Naman Arora <namanarora1022@gmail.com>
2024-08-28 19:19:03 +0530

    fix: 🔨 web sockets

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,11 @@ import 'dotenv/config';
 import express, { Application } from 'express';
 import cors from 'cors';
 import { homeRoutes } from './routes';
-import { getSocket, initSocket } from './common/socket';
 import { createServer } from 'http';
+import path from 'path';
+import imageToBase64 from 'image-to-base64';
+import { charToImage } from './utils';
+import { Server } from 'socket.io';
 
 const main = async () => {
     // CONSTANTS
@@ -11,22 +14,47 @@ const main = async () => {
     const port = process.env.PORT ?? 5000;
 
     // MIDDLEWARE
-    app.use(express.json());
+    app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ extended: false }));
     app.use(cors({ origin: '*' }));
+    app.use(express.static(path.join(__dirname, '..', 'public')));
 
     // ROUTES
     app.use('/', homeRoutes);
 
     // WEB SOCKETS
     const server = createServer(app);
-    initSocket(server);
-    const io = getSocket();
+    const io = new Server(server);
     io.on('connection', (socket) => {
         console.log('Connected', socket.id);
 
         socket.on('disconnect', () => {
             console.log('Disconnected');
+        });
+
+        // EVENTS
+
+        // 1. Text to Image
+        socket.on('text-stream', ({ text }: { text: string }) => {
+            let index = 0;
+
+            const intervalId = setInterval(async () => {
+                if (index >= text.length) {
+                    clearInterval(intervalId);
+                    return;
+                }
+
+                const char = text[index];
+
+                try {
+                    const base64 = await imageToBase64(charToImage(char));
+                    socket.emit('image-stream', { image: base64 });
+                } catch (error) {
+                    console.error(error);
+                }
+
+                index++;
+            }, 2000); // 2 seconds
         });
     });
 
