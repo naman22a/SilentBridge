21e85c000501baff072503b2e91d818ae8b6349e
himanshutiwari2005 <himanshutiwari10112005@gmail.com>
2024-08-30 16:53:41 +0530

    Create main.py

---
+++ model/main.py
@@ -0,0 +1,14 @@
+# Import libraries as required
+
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+from keras import layers, models
+
+# Data Import and Handling
+
+
+
+
+Class_names = []
+
