dfc64ab08094d5ba07dd58a6c3b8312149351811
Naman Arora <namanarora1022@gmail.com>
2024-09-01 14:53:39 +0530

    feat: add ISL to audio route

---
+++ app/app/ISL.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ISL = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ISL</Text>
+    </View>
+  );
+};
+
+export default ISL;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    justifyContent: "center",
+  },
+});
